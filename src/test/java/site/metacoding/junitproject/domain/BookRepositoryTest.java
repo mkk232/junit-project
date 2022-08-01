be8570e0912f0aca705fb7bc7788f568b38ceef9
Minki <mkk232@naver.com>
2022-08-01 21:42:12 +0900

    Repository Test -책 삭제하기 버그 수정

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,8 +1,10 @@
 package site.metacoding.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 // Controller -> Service -> Repository
 //  (3)             (2)         (1) DB쪽 관련 테스트
@@ -33,8 +36,8 @@ public class BookRepositoryTest {
                         .build();
         bookRepository.save(book);
     }
-    // 가정 1 : [데이터준비() + 1 책등록] (T), [데이터준비 + 2 책목록보기] (T)
-    // 가정 2 : [데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기] (T)
+    // 가정 1 : [데이터준비() + 1 책등록] (T), [데이터준비 + 2 책목록보기] (T) ==> 사이즈 1 (검증 완료)
+    // 가정 2 : [데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기] (T) ==> 사이즈 2 (검증 실패)
 
     // 1. 책 등록
     @Test
@@ -76,6 +79,7 @@ public class BookRepositoryTest {
     }// 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
     @Test
     public void 책한건보기_test() {
         // given
@@ -89,8 +93,41 @@ public class BookRepositoryTest {
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
     }
-// 4. 책 수정
-// 5. 책 삭제
+    // 4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test() {
+        // given
+        Long id = 1L;
+
+        // when
+        bookRepository.deleteById(id);
+
+        // then
+        // Optional<Book> bookPS = bookRepository.findById(id).isPresent();
+
+        // isPresent() = null이 아니면, 존재한다면
+        // assertFalse = false면 성공, true면 실패
+        assertFalse(bookRepository.findById(id).isPresent());
+
+    }
+
+    // 5. 책 수정
 
 }
  
+/*
+ * JUnit 테스트
+ * 
+ * 1. 테스트 메서드 3개가 있다. (순서 보장이 안된다.) - Order()어노테이션을 사용해야함
+ * (1) 메서드1
+ * (2) 메서드2
+ * (3) 메서드3
+ * 가장 위에있다고 먼저 실행되는거 아님. 
+ * 
+ * 2. 테스트 메서드가 하나 실행 후 종료되면 데이터가 초기화된다. - Transactional() 어노테이션
+ * (1) 1건
+ * (2) 2건
+ * -> 트랜잭션 종료 -> 데이터 초기화
+ * *** primary key auto_increment 값이 초기화가 안됨. 
+ */
